aa129d240e7094eefaeda933b2639df35f4f2b5f
Vaporise <draxdragonfire@googlemail.com>
2025-01-10 00:05:17 +0900

    Added inputs, type command, echo command and command not found

---
+++ main.py
@@ -0,0 +1,38 @@
+import sys
+
+
+def main():
+
+    commands = ["paper", "rock", "scissors"]
+    builtins = ["echo", "exit", "type"]
+    #Uncomment this block to pass the first stage
+    while True: 
+        sys.stdout.write("$ ")
+
+        #Wait for user input
+        inp = input()
+
+        if inp in commands: # checks if the command is in the list
+            print(f"{inp}: command found")
+        elif inp.startswith("type "):
+            target = inp[5:]
+            if target == "echo":
+                sys.stdout.write("echo is a shell builtin\n")
+            elif target == "exit":
+                sys.stdout.write("exit is a shell builtin\n")
+            elif target == "type":
+                sys.stdout.write("type is a shell builtin\n")
+            else: 
+                sys.stdout.write(f"{target}: not found\n")
+
+
+        elif inp.startswith("echo "): #if the input starts with echo
+            sys.stdout.write(inp[5:] + "\n") #skip the first 5 letters and then print.
+        elif inp == "exit 0": # exits the infinite loop
+            break
+        else: # if command is not in the list
+            print(f"{inp}: command not found")
+        
+
+if __name__ == "__main__":
+    main()
