aba1a4d19b32e7b533066e59edd0ba2e0e6d0bd9
Vaporise <draxdragonfire@googlemail.com>
2025-01-20 23:54:27 +0900

    m

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import sys
+import os
 
 
 def main():
 
     commands = ["paper", "rock", "scissors"]
     # builtins = ["echo", "exit", "type"] due to code change no longer needed
+
+    path_dirs = os.environ.get("PATH", "").split(":")
+    if "/bin" not in path_dirs:
+        path_dirs.append("/bin")
+    if "/usr/bin" not in path_dirs:
+        path_dirs.append("/usr/bin")
 
     while True: 
         sys.stdout.write("$ ")
@@ -14,24 +21,37 @@ def main():
 
         if inp in commands: # checks if the command is in the list
             print(f"{inp}: command found")
-        elif inp.startswith("type "):
-            target = inp[5:]
-            if target == "echo":
+        elif inp.startswith("type "): #check if command starts with type for using the type command
+            target = inp[5:] #ignores the first 5 characters (type ) and assigns all the string after.
+            if target == "echo": 
                 sys.stdout.write("echo is a shell builtin\n")
             elif target == "exit":
                 sys.stdout.write("exit is a shell builtin\n")
             elif target == "type":
                 sys.stdout.write("type is a shell builtin\n")
+
+                found = False
+                for directory in path_dirs:
+                    full_path = os.path.join(directory, target)
+                    if os.path.exists(full_path) and os.access(full_path, os.X_OK):
+                        sys.stdout.write(f"{target} is {full_path}\n")
+                        found = True
+                        break
+                if not found:
+                    sys.stdout.write(f"{target}: not found\n")
+
             else: 
                 sys.stdout.write(f"{target}: not found\n")
 
 
         elif inp.startswith("echo "): #if the input starts with echo
             sys.stdout.write(inp[5:] + "\n") #skip the first 5 letters and then print.
-        elif inp == "exit 0": # exits the infinite loop
+        elif inp == "exit": # exits the infinite loop
             break
         else: # if command is not in the list
             print(f"{inp}: command not found")
+        
+        
         
 
 if __name__ == "__main__":
